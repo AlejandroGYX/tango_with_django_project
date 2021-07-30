318355234c31d41b39fd2306333fadddadd8fe8c
Yuxin Guo <2598557g@student.gla.ac.uk>
2021-07-30 08:29:54 +0100

    Chapter_9 Modify commit by 2598557G_Yuxin Guo

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from rango.models import Category, Page
 from rango.models import UserProfile
@@ -11,18 +10,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
-=======
-from django.contrib import admin
-from rango.models import Category, Page
-from rango.models import UserProfile
-
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page, PageAdmin)
->>>>>>> d9b7db645dc099b0a1ce863a25ffc93813f5dcdc
 admin.site.register(UserProfile)
--- rango/forms.py
+++ rango/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import forms
 from rango.models import Page, Category
 from django.contrib.auth.models import User
@@ -43,50 +42,4 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-=======
-from django import forms
-from rango.models import Page, Category
-from django.contrib.auth.models import User
-from rango.models import UserProfile
-
-class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-
-    class Meta:
-        model = Category
-        fields = ('name',)
-
-class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-
-    class Meta:
-        model = Page
-        exclude = ('category',)
-    
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-
-        if url and not url.startswith('http://'):
-            url = f'http://{url}'
-            cleaned_data['url'] = url
-        
-        return cleaned_data
-
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password',)
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
->>>>>>> d9b7db645dc099b0a1ce863a25ffc93813f5dcdc
         fields = ('website', 'picture',)
