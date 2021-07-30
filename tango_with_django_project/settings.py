34c59d6943868201f20cdd887cb6d92c9ab72239
Yuxin Guo <2598557g@student.gla.ac.uk>
2021-07-30 08:53:04 +0100

    Chapter_10 commit by 2598557G_Yuxin Guo

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'rango:login'
 
 # Application definition
 
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, },
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
